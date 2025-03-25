393d98bd9aa0de6644ac7a226e6db5ae1319101f
Kusuma829 <245122749006@mvsrec.edu.in>
2025-03-25 15:40:17 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class add{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("addition="+(a+b));
+System.out.println("subtraction="+(a-b));
+System.out.println("multiplication="+(a*b));
+System.out.println("division="+(a/b));
+}
+}
