30dcd70ad20452a730977a2a784ec270e19fc37b
Kusuma829 <245122749006@mvsrec.edu.in>
2025-03-25 14:52:25 +0530

    Add files via upload

---
+++ arth.java
@@ -0,0 +1,10 @@
+class arth{
+public static void main(String[] args){
+int a=30;
+int b=5;
+System.out.println("addition="+(a+b));
+System.out.println("subtraction="+(a-b));
+System.out.println("multiplication="+(a*b));
+System.out.println("division="+(a/b));
+}
+}
