38b6886d90fe0d72b72c56510a09143c5b8d85e9
Kusuma829 <245122749006@mvsrec.edu.in>
2025-03-29 14:46:23 +0530

    Add files via upload

---
+++ Stud.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Stud{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+String name=sc.nextLine();
+//int age=sc.nextInt();
+//int roll=sc.nextInt();
+int age=Integer.parseInt(args[0]);
+int roll=Integer.parseInt(args[1]);
+System.out.println("name:"+name);
+System.out.println("age:"+age);
+System.out.println("roll"+roll);
+}
+}
+
