19b4d7d59490b01ef203873fd1c7cce88cec4e37
Kusuma829 <245122749006@mvsrec.edu.in>
2025-03-29 14:26:27 +0530

    add.java

--- add.java
+++ add.java
@@ -2,7 +2,6 @@ import java.util.*;
 class add{
 public static void main(String args[])
 {
-Scanner sc=new Scanner(System.in);
 int a=Integer.parseInt(args[0]);
 int b=Integer.parseInt(args[1]);
 System.out.println("addition="+(a+b));
