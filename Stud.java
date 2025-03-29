0459f95a08797d95c961f2e8247445bb40542ecb
Kusuma829 <245122749006@mvsrec.edu.in>
2025-03-29 14:52:47 +0530

    Stud.java

--- Stud.java
+++ Stud.java
@@ -6,7 +6,7 @@ String name=args[0];
 //int age=sc.nextInt();
 //int roll=sc.nextInt();
 int age=args[1];
-int roll=args[1];
+int roll=args[2];
 System.out.println("name:"+name);
 System.out.println("age:"+age);
 System.out.println("roll"+roll);
