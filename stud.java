c3e0776e103099a7a9e39346dc800a0fcbb4c120
Kusuma829 <245122749006@mvsrec.edu.in>
2025-03-29 14:58:23 +0530

    stud.java

--- stud.java
+++ stud.java
@@ -3,8 +3,8 @@ class stud{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 String name=sc.nextLine();
-int age=sc.nextInt();
-int roll=sc.nextInt();
+String age=sc.nextLine();
+String  roll=sc.nextLine();
 //int age=Integer.parseInt(args[0]);
 //int roll=Integer.parseInt(args[1]);
 System.out.println("name:"+name);
